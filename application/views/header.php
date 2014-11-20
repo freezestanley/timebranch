96bb517a84c9de7e1828d9025933c7a974d7ab8b
xu.xiang@dena.jp <xu.xiang@dena.jp>
2014-11-20 18:15:27 +0800

    xxx

---
+++ application/views/header.php
@@ -0,0 +1,11 @@
+<title>DeNA GAME TOOLS</title>
+<meta charset="utf-8">
+<link rel="shortcut icon" href="/favicon.png?v=2" type="image/png">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="apple-itunes-app" content="">
+<meta name="renderer" content="webkit">
+<meta name="author" content="xx">
+<meta http-equiv="Cache-Control" content="no-siteapp">
+<script type="text/javascript" src='<?php echo js_url('jquery.min.js'); ?>'></script> 
+<script type="text/javascript" src='<?php echo js_url('angular.js'); ?>'></script> 
+<script type="text/javascript" src='<?php echo js_url('bootstrap.min.js'); ?>'></script> 
