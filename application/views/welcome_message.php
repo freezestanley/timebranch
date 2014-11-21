2ad8c1755f02da0d63b02ef7b361e4bfd4ce0e48
xu.xiang@dena.jp <xu.xiang@dena.jp>
2014-11-21 16:29:09 +0800

    xx

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -50,13 +50,19 @@
   </div>
   <div id="container" style="min-width:700px;height:400px"></div>
   <div>
-    <div class="row">
+  
+  <div class="panel panel-primary">
+      <div class="panel-heading">
+        <h3 class="panel-title">2014年第1季</h3>
+      </div>
+      <div class="panel-body">
+         <div class="row">
       <div class="col-sm-6 col-md-4">
         <div class="thumbnail">
           <div class="caption">
             <h3>Thumbnail label</h3>
             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            
           </div>
         </div>
       </div>
@@ -65,7 +71,7 @@
           <div class="caption">
             <h3>Thumbnail label</h3>
             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            
           </div>
         </div>
       </div>
@@ -74,7 +80,7 @@
           <div class="caption">
             <h3>Thumbnail label</h3>
             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            
           </div>
         </div>
       </div>
@@ -83,7 +89,7 @@
           <div class="caption">
             <h3>Thumbnail label</h3>
             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            
           </div>
         </div>
       </div>
@@ -92,11 +98,36 @@
           <div class="caption">
             <h3>Thumbnail label</h3>
             <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-            <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+           
           </div>
         </div>
       </div>
     </div>
+      </div>
+    </div>
+  
+  
+  
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
   </div>
   <div align="center">
     <div class="btn-group">
