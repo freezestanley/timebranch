110d9b68879b9decd3ac3473f1a4c6284159b740
cloverstd <cloverstd@gmail.com>
2014-11-26 14:39:00 +0800

    controller init

---
+++ application/controllers/api.php
@@ -0,0 +1,113 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class API extends CI_Controller {
+
+    public function json($data='', $status=TRUE, $err_msg='')
+    {
+        $result = array('data'=>$data, 'status'=>$status);
+        if ($status !== TRUE) {
+            $result['err_msg'] = $err_msg;
+        }
+        header('Content-Type: application/json');
+        echo json_encode($result);
+        exit;
+    }
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->load->model('project_model', 'project');
+        $this->load->model('node_model', 'node');
+        $this->load->model('update_model', 'update');
+        $this->load->model('type_model', 'type');
+    }
+
+    // public function index()
+    // {
+    //     $this->load->view('welcome_message');
+    // }
+
+    public function filter()
+    {
+        $project_name = $this->input->get('name');
+        $project_os = $this->input->get('os');
+        $project_area = $this->input->get('area');
+        $project_startime = $this->input->get('startime');
+        $project_deadtime = $this->input->get('deadtime');
+        $page = $this->input->get('p');
+        if (empty($page)) {
+            $page = 1;
+        }
+
+        $condition = array('name' => $project_name,
+                           'os' => $project_os,
+                           'area' => $project_area,
+                           'startime' => $project_startime,
+                           'deadtime' => $project_deadtime,
+                           );
+
+        $projects = $this->project->filter($condition);
+        $page_data = $this->_paginate($this->_filter($projects), $page, $per_page=2);
+        $this->json($page_data);
+    }
+
+    public function _filter($projects)
+    {
+        $result = array();
+        $_temp = array();
+        foreach ($projects as $project) {
+            if (isset($_temp[$project->pid])) {
+                $_node = array('nid' => $project->nid,
+                               'remark' => $project->n_remark,
+                               'update_remark' => $project->u_remark,
+                               // 'tid' => $project->tid,
+                               'type' => $project->n_type,
+                               'deadline' => $project->deadline_time,
+                               'updatetime' => $project->update_time,
+                               );
+                $_temp[$project->pid]['nodes'][] = $_node;
+            } else {
+                $_node = array('nid' => $project->nid,
+                               'remark' => $project->n_remark,
+                               'update_remark' => $project->u_remark,
+                               // 'tid' => $project->tid,
+                               'type' => $project->n_type,
+                               'deadline' => $project->deadline_time,
+                               'updatetime' => $project->update_time,
+                               );
+                if ($project->nid == null) {
+                    $nodes = array();
+                } else {
+                    $nodes = array($_node);
+                }
+                $_temp[$project->pid] = array('nodes'=>$nodes,
+                                              'name'=>$project->name,
+                                              'os'=>$project->os,
+                                              'area'=>$project->area,
+                                              'ip'=>$project->ip,
+                                              'contract'=>$project->contract,
+                                              'issuer'=>$project->issuer,
+                                              'issuer'=>$project->issuer,
+                                              'type'=>$project->p_type,
+                                              'operator'=>$project->operator,
+                                              'apply'=>$project->apply,
+                                              'remark'=>$project->p_remark,
+                                              );
+            }
+        }
+
+        return $_temp;
+    }
+
+    public function _paginate($data, $page, $per_page)
+    {
+        return array_slice($data, ($page-1)*$per_page, $per_page);
+    }
+
+    public function get_projects()
+    {
+        $projects = $this->project->get_projects();
+        $this->json($projects);
+    }
+}
---
+++ application/controllers/project.php
@@ -0,0 +1,86 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Project extends MY_Controller {
+
+    function __construct()
+    {
+        parent::__construct();
+
+        $this->check_login();
+        if (!$this->check_admin()) {
+            redirect(base_url());
+        }
+    }
+
+    public function index()
+    {
+        $this->load->view('welcome_message');
+    }
+
+    public function add()
+    {
+        $data = array('title' => 'add project');
+
+        $this->form_validation->set_rules('name', 'Name', 'required');
+        $this->form_validation->set_rules('os[]', 'OS', 'required');
+        $this->form_validation->set_rules('area[]', 'area', 'required');
+        $this->form_validation->set_rules('producer[]', 'producer', 'required');
+
+        if ($this->form_validation->run() == FALSE)
+        {
+
+            $this->load->view('admin/header', $data);
+            $this->load->view('admin/project_add');
+            $this->load->view('admin/footer');
+        }
+        else
+        {
+            $this->load->model('project_model', 'project');
+            $this->project->insert();
+            echo 'ok';
+        }
+    }
+
+    public function test_show($page=1)
+    {
+        $this->load->model('project_model', 'project');
+        $per_page = 5;
+        $this->load->library('pagination');
+
+        $config['base_url'] = base_url().'project/test_show';
+        $config['total_rows'] = $this->project->count();
+        $config['per_page'] = $per_page;
+        $config['use_page_numbers'] = TRUE;
+        $config['uri_segment'] = 3;
+
+        $this->pagination->initialize($config);
+
+        $projects = $this->project->get($page, $per_page);
+        $data = array();
+        $data['pagination'] = $this->pagination->create_links();
+        $data['projects'] = $projects;
+
+        $this->load->view('admin/header', $data);
+        $this->load->view('admin/test_show');
+        $this->load->view('admin/footer');
+
+    }
+
+    public function test_show_id($project_id)
+    {
+        $this->load->model('project_model', 'project');
+        $this->load->model('node_model', 'node');
+        $project = $this->project->get_by_id($project_id);
+        if (empty($project)) {
+            show_404();
+        }
+
+        $nodes = $this->node->get_by_pid($project_id);
+        $data = array('project'=>$project,
+                      'nodes' => $nodes);
+        $this->load->view('admin/header', $data);
+        $this->load->view('admin/test_show_id');
+        $this->load->view('admin/footer');
+
+    }
+}
