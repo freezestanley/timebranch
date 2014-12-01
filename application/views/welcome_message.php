012b3d1f4edce17dfe700912c429ccb1b9bff188
xu.xiang@dena.jp <xu.xiang@dena.jp>
2014-12-01 20:21:00 +0800

    xx

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -80,7 +80,7 @@
 
 
  <!-- <div class="gantt"></div>-->
-  <iframe src="<?=site_url()?>api/gantt_iframe" style="width:100%; border:none; height:525px;" scrolling="no"></iframe>
+  <iframe src="<?=site_url()?>api/gantt_iframe" style="width:100%; border:none; height:525px; margin-bottom:30px;" scrolling="no"></iframe>
 
 
 
