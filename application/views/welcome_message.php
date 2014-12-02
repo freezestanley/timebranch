0c5def7ee019273ee5385872c5b4dfb05b7d6057
xu.xiang@dena.jp <xu.xiang@dena.jp>
2014-12-02 11:30:16 +0800

    xx

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -236,7 +236,7 @@
 <div class="dialog" id="dialog" ng-controller="dialogController" ng-show="showDia">
 		<div class="dia_zone" id="dia_zone">
         <a href="javascript:;" ng-click="showDia = showDia == true?false:true;" class="close"></a>
-			<table class="p_table">
+			<table class="p_table"  >
             	<tr><td>项目时间</td><td>修改操作</td><td>修改时间</td><td>修改原因</td></tr>
             	<tr ng-repeat="h in history"><td >{{h.update_time}}</td><td>{{h.type | typefilter}}</td><td>{{h.addtime}}</td><td>{{h.remark}}</td></tr>
             </table>
